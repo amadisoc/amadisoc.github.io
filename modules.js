8cef11ad3af692f3971ff656834d0375726e22fe
amadisoc <all15005@byui.edu>
2016-11-05 21:05:14 -0600

    Broken
    Not sure why it's not working.

---
+++ modules.js
@@ -0,0 +1,11 @@
+$(function() {
+$("#MyHeader").load("header.html");
+});
+$(function() {
+$("#MyNav").load("navigation.html");
+});
+$ (function() {
+$("#MyFooter").load("footer.html");
+});
+$("#MyContent").load("content.html");
+});
